db7668e636332121ecdeb351d2ce06c4c29ee568
Muhammad Abdullah <121270279+abdullah2k05@users.noreply.github.com>
2025-07-22 22:12:16 +0500

    Update Log_File_Splitter_using_stream.js

--- Log_File_Splitter_using_stream.js
+++ Log_File_Splitter_using_stream.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const readline = require("readline");
 
-const ReadStream = fs.createReadStream("./Log_file_splitter_server.log");
-const WriteStream = fs.createWriteStream("log_file_program_output.log");
+const ReadStream = fs.createReadStream("./Log_file_splitter_server.log"); //input file name
+const WriteStream = fs.createWriteStream("log_file_program_output.log");  //output file name
 
 const rl = readline.createInterface({
   input: ReadStream,
